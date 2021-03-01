41451052b8a60dbf7bd8a20be728bca761f01ad4
ykpark <read0more@gmail.com>
2021-03-01 16:18:51 +0900

    Add: 왼쪽 aside 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.css";
+import Aside from "./components/Aside/Aside";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className={styles.app}>
+      <Aside />
+      <main>main</main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Aside/Aside.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Aside.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faImage,
+  faStickyNote,
+  faTasks,
+} from "@fortawesome/free-solid-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+
+const Aside = () => (
+  <aside className={styles.aside}>
+    <FontAwesomeIcon icon={faImage} className={styles.logo} />
+    <FontAwesomeIcon icon={faYoutube} className={styles.logo} />
+    <FontAwesomeIcon icon={faStickyNote} className={styles.logo} />
+    <FontAwesomeIcon icon={faTasks} className={styles.logo} />
+  </aside>
+);
+
+export default Aside;
