048e5df9f6cc446595b09ac3aec1f7f9606f1360
ykpark <read0more@gmail.com>
2021-03-01 19:44:25 +0900

    Add: note에 추가할 항목 type정의

---
+++ src/common/types.ts
@@ -0,0 +1,21 @@
+export type Image = {
+  type: "image";
+  title: string;
+  url: string;
+};
+
+export type Video = Omit<Image, "type"> & {
+  type: "video";
+};
+
+export type Note = {
+  type: "note";
+  title: string;
+  body: string;
+};
+
+export type Task = Omit<Note, "type"> & {
+  type: "task";
+};
+
+export type Item = Image | Video | Note | Task;
