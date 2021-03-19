c9bed800615084a1ce9dbfbf68eb44a4e98465c0
ykpark <read0more@gmail.com>
2021-03-19 23:07:46 +0900

    Refactor: youtube url 정규식 수정

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1,5 +1,7 @@
 export function getYoutubeVideoIdFromURL(url: string): string {
-  const [_, videoId] = url.split(/^https?:\/\/youtu.be\//i);
+  const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+  const match = url.match(regExp);
+  const videoId = match ? match[1] || match[2] : undefined;
 
   if (!videoId) {
     throw Error("youtube URL을 확인해 주세요.");
